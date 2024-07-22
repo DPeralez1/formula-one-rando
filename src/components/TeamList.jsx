235816a7232fdaac9b460b7bf3a79f035f1c4e78
David Peralez <davidperalez89@gmail.com>
2024-07-22 12:56:32 -0700

    added images to all the teams

--- src/components/TeamList.jsx
+++ src/components/TeamList.jsx
@@ -1,54 +1,62 @@
 export const TEAM_LIST = [
   {
     id: 1,
-    team_name: "Mercedes-AMG Petronas Formula One Team",
+    team_name: "Mercedes",
     image:
       "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/mercedes",
   },
   {
     id: 2,
-    team_name: "Oracle Red Bull Racing",
+    team_name: "Red Bull Racing",
     image:
       "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/red%20bull",
   },
   {
     id: 3,
-    team_name: "Scuderia Ferrari",
-    // image: Ferrari,
+    team_name: "Ferrari",
+    image:
+      "https://media.formula1.com/image/upload/f_auto,c_limit,q_auto,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/ferrari",
   },
   {
     id: 4,
-    team_name: "McLaren Formula One Team",
-    // image: Mclaren,
+    team_name: "McLaren",
+    image:
+      "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/mclaren",
   },
   {
     id: 5,
-    team_name: "Aston Martin Aramco Formula One Team",
-    // image: AstonMartin,
+    team_name: "Aston Martin",
+    image:
+      "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/aston%20martin%202024",
   },
   {
     id: 6,
-    team_name: "BWT Alpine F1 Team",
-    // image: Alpine,
+    team_name: "Alpine",
+    image:
+      "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/alpine",
   },
   {
     id: 7,
-    team_name: "Stake F1 Team Kick Sauber",
-    // image: Stake,
+    team_name: "Kick Sauber",
+    image:
+      "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/kick%20sauber",
   },
   {
     id: 8,
-    team_name: "Visa Cash App RB Formula One Team",
-    // image: RB,
+    team_name: "RB",
+    image:
+      "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/rb",
   },
   {
     id: 9,
-    team_name: "Williams Racing",
-    // image: Williams,
+    team_name: "Williams",
+    image:
+      "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/williams",
   },
   {
     id: 10,
-    team_name: "MoneyGram Haas F1 Team",
-    // image: Haas,
+    team_name: "Haas",
+    image:
+      "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/haas",
   },
 ];
