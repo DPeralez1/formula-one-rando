002e24027cfe7c0c38db367e366ce6e59a53d6bb
David Peralez <davidperalez89@gmail.com>
2024-07-22 16:42:16 -0700

    Styling
    installed React Bootstrap and changed the button styles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import RandomTeam from "./components/RandomTeam";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
--- src/components/RandomTeam.jsx
+++ src/components/RandomTeam.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { TEAM_LIST } from "../components/TeamList";
 import { DRIVER_LIST } from "../components/DriverList";
+import { Button } from "react-bootstrap";
 
 export default function RandomTeam() {
   const [team, setTeam] = useState({});
@@ -23,10 +24,16 @@ export default function RandomTeam() {
     <div className="random-team-container">
       <h1>Formula One Team Randomizer!</h1>
       <h2>You can either choose a team or generate a random driver!!!!</h2>
-      <button onClick={handleGenerateTeam}>Generate Team</button>
-      <button onClick={handleGenerateDriver}>Generate Driver</button>
+      <Button variant="danger" onClick={handleGenerateTeam}>
+        Generate Team
+      </Button>
+      <Button variant="warning" onClick={handleGenerateDriver}>
+        Generate Driver
+      </Button>
       <h3 className="display-team">Team: {team.team_name}</h3>
-      <div>{team.image && <img src={team.image} alt={team.team_name} />}</div>
+      <div className="display-team-image">
+        {team.image && <img src={team.image} alt={team.team_name} />}
+      </div>
       <h3 className="display-driver">Driver: {driver.driver_name}</h3>
       <div>
         {driver.driver_image && (
